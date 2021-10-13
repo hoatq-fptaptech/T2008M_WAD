2e6192e2335a9c9ba922a1705569b2acf504bb7e
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-10-13 11:00:08 +0700

    entity

--- T2008M_WAD/Controllers/TestController.cs
+++ T2008M_WAD/Controllers/TestController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using T2008M_WAD.Models;
 namespace T2008M_WAD.Controllers
 {
     public class TestController : Controller
@@ -11,7 +11,24 @@ namespace T2008M_WAD.Controllers
         // GET: Test
         public ActionResult Demo()
         {
-            return View();
+            ViewData["xgame"] = 13;
+            ViewBag.XGame = 26;
+            var day = 13;
+            return View(day);// compact data - chi truyen dc 1 object
+        }
+
+        public ActionResult ListCategory()
+        {
+            DataContext dc = new DataContext();
+            List<Category> categories = dc.Categories.ToList();
+           //categories.Add(new Category() { Name="Fashions" });
+            return View(categories);
+        }
+
+        public ActionResult Form()
+        {
+            var c = new Category() { Name = "Fashions" };// neu co compact thi form se nap data vao input
+            return View(c);
         }
     }
 }
---
+++ T2008M_WAD/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace T2008M_WAD.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(): base("T2008M") { 
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
